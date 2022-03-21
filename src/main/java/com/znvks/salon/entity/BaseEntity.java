e76e7349b91637cd6100523fbd4a4a6b72474eff
Kristina <isakovakristina666@gmail.com>
2022-03-21 17:34:27 +0300

    update base entity

--- src/main/java/com/znvks/salon/entity/BaseEntity.java
+++ src/main/java/com/znvks/salon/entity/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.znvks.salon.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
+@Setter
 @ToString
 @MappedSuperclass
 public abstract class BaseEntity<PK extends Serializable> {
